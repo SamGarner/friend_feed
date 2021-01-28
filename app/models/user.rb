777e27cacaab580c757ef5ab9eb8354b2f56b637
SamGarner <samuel.w.garner@gmail.com>
2021-01-28 10:35:57 -0600

    Fix: Update User to not contain :confirmable or :locakable Devise modules.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :lockable and :omniauthable
+  # :confirmable, :lockable and :omniauthable
   validates :email, :name, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable # non-default modeuls
+         :timeoutable, :trackable # non-default modeuls
 
   has_many :posts
 end
