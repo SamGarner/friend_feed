6a6186f7758be83bd2337d2b109dd51ea2cfd32f
SamGarner <samuel.w.garner@gmail.com>
2021-01-27 18:35:07 -0600

    Feat: Add User model with Devise modules and presence validations.

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable and :omniauthable
+  validates :email, :name, presence: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable, :trackable # non-default modeuls
+
+end
