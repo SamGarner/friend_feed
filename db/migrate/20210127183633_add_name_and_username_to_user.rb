2cb08defce9f74dc63a72751ab9b0db5a9316b81
SamGarner <samuel.w.garner@gmail.com>
2021-01-27 18:36:15 -0600

    Feat: Add/run migrations for User model.

---
+++ db/migrate/20210127183633_add_name_and_username_to_user.rb
@@ -0,0 +1,7 @@
+class AddNameAndUsernameToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :name, :text
+    add_column :users, :username, :text
+    add_index :users, :username, unique: true
+  end
+end
