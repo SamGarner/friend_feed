a6ee8fc0ccfe59e2d514e44f5fda394898b6d6c0
SamGarner <samuel.w.garner@gmail.com>
2021-01-28 10:37:26 -0600

    Feat: Add User migration for Devise trackable fields, add User db seeds.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_005948) do
+ActiveRecord::Schema.define(version: 2021_01_28_163147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2021_01_28_005948) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "name"
     t.text "username"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+User.create([{ name: 'Iris Noodle', email: 'iris@noodle.com', username: 'noodle',
+               password: 'Password2', password_confirmation: 'Password2' },
+             { name: 'Chewbert', email: 'chewy@noodle.com', username: 'chewy',
+               password: 'Password2', password_confirmation: 'Password2' },
+             { name: 'Ben Steve', email: 'ben@path.com', username: 'steven',
+               password: 'Password2', password_confirmation: 'Password2' },
+             { name: 'Roo', email: 'roo@noodle.com', username: 'roo',
+               password: 'Password2', password_confirmation: 'Password2' }])
