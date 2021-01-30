dea521bc57b390c922bd7acb761698ec7547d526
SamGarner <samuel.w.garner@gmail.com>
2021-01-30 15:52:50 -0600

    Feat: Add User/Friendship associations.

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
   validates :sender_id, :receiver_id, presence: true
   validates :sender_id, uniqueness: { scope: :receiver_id }
+
+  belongs_to :sender, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
 end
