aa80eba67519fb9099cd2b40aaca5ea5971c96be
SamGarner <samuel.w.garner@gmail.com>
2021-01-29 21:00:42 -0600

    Feat: Add uniqueness validations to Friendship and FriendRequest models.

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,5 +1,6 @@
 class FriendRequest < ApplicationRecord
   validates :sender_id, :receiver_id, presence: true
+  validates :sender_id, uniqueness: { scope: :receiver_id }
 
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,4 @@
 class Friendship < ApplicationRecord
+  validates :sender_id, :receiver_id, presence: true
+  validates :sender_id, uniqueness: { scope: :receiver_id }
 end
