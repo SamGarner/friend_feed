01f7843677c91bba46fdd9a766807fbd12747b5e
SamGarner <samuel.w.garner@gmail.com>
2021-01-29 20:08:06 -0600

    Feat: Update FriendRequestController to fetch sent friend requests in #index; update #create to redirect to friend_requests_path after save.

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,18 +1,22 @@
 class FriendRequestsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    redirect_back(fallback_location: root_path)
+    @current_user = current_user
+    @requests_sent = FriendRequest.pending_sent_requests(@current_user.id)
   end
 
   def create
     @new_request = FriendRequest.new(friend_request_params)
     if @new_request.save
-      flash.now[:success] = 'Friend request sent!'
+      flash[:success] = 'Friend request sent!'
+      redirect_to friend_requests_path
     else
       flash.now[:failure] = 'Error - Please try again'
       @friend_request = FriendRequest.new
+      @user = User.find(params[:friend_request][:receiver_id])
+      render 'users/show'
     end
-    @user = User.find(params[:friend_request][:receiver_id])
-    render 'users/show'
   end
 
   private
