b148599ce65534554f96e66ce6ceb2878da13e48
SamGarner <samuel.w.garner@gmail.com>
2021-01-29 20:09:17 -0600

    Feat: Add #pending_sent_requests to find all sent friend requests for the user.

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -3,4 +3,8 @@ class FriendRequest < ApplicationRecord
 
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
+
+  def self.pending_sent_requests(user_id)
+    FriendRequest.where('sender_id = ?', user_id)
+  end
 end
