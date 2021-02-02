ee3f10d8b0854b4e80542817fb070d2a19144a2d
SamGarner <samuel.w.garner@gmail.com>
2021-02-01 19:02:55 -0600

    Deprecate FriendRequest: Remove FriendRequest associations from User model, functionality move to Friendship.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
          :timeoutable, :trackable # non-default modeuls
 
   has_many :posts
-  has_many :sent_requests, foreign_key: 'sender_id', class_name: 'FriendRequest'
-  has_many :received_requests, foreign_key: 'receiver_id', class_name: 'FriendRequest'
+  # has_many :sent_requests, foreign_key: 'sender_id', class_name: 'FriendRequest'
+  # has_many :received_requests, foreign_key: 'receiver_id', class_name: 'FriendRequest'
   has_many :sent_friendships, foreign_key: 'sender_id', class_name: 'Friendship'
   has_many :received_friendships, foreign_key: 'receiver_id', class_name: 'Friendship'
   has_many :notifications
