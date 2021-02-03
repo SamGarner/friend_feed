0b11417911b4070e0e4bd3fb8951d51a12f56562
SamGarner <samuel.w.garner@gmail.com>
2021-02-03 11:18:53 -0600

    Feat: Add LikesController with #create action.

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,18 @@
+class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.build(likes_params)
+    if @like.save
+      #correct reach/violater of Demeter's Law to get User's name?
+      flash[:notice] = "Liked #{@like.likable.user.name}'s #{@like.likable_type.downcase}."
+    else
+      flash[:alert] = "Your 'like' was not saved. Please try again."
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def likes_params
+    params.require(:like).permit(:likable_id, :likable_type)
+  end
+end
