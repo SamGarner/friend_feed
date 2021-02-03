5379adfa99bbe53364028b84ab7fc8f9690ed443
SamGarner <samuel.w.garner@gmail.com>
2021-02-03 11:15:40 -0600

    Feat: Add three-key unique index to Likes table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_233539) do
+ActiveRecord::Schema.define(version: 2021_02_03_030345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_233539) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["likable_type", "likable_id"], name: "index_likes_on_likable"
+    t.index ["user_id", "likable_id", "likable_type"], name: "index_likes_on_user_id_and_likable_id_and_likable_type", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
