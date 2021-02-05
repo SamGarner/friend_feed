ffa0d3d98c9474655fffe3453284c096ec01a975
SamGarner <samuel.w.garner@gmail.com>
2021-02-05 15:32:22 -0600

    Feat: Update User model for facebook omniauth and override user routing for omniauth callbacks.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :timeoutable, :trackable # non-default modeuls
+         :timeoutable, :trackable, # non-default modules
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :posts
   has_many :comments
@@ -20,4 +21,29 @@ class User < ApplicationRecord
   def self.fetch_stranger_ids(connected_user_ids)
     User.where.not(id: connected_user_ids).map(&:id)
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.provider = auth.provider 
+  #     user.uid      = auth.uid
+  #     user.name     = auth.info.name
+  #     user.save
+  #   end
+  # end
+
+      #   from_omniauth(auth)
+      # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      # user.email = auth.info.email
+      # user.password = Devise.friendly_token[0,20]
+      # user.name = auth.info.name # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
 end
